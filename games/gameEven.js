115f2bc131e75f48fecac75fbbba3b24b0bc00e2
satania-lee <rockky.tl@gmail.com>
2022-07-17 11:28:18 +0300

    fix dublicate

--- games/gameEven.js
+++ games/gameEven.js
@@ -3,13 +3,11 @@ import randomNumberGenerator from '../src/randomNumberGenerator.js';
 
 const isEven = (number) => number % 2 === 0;
 
-const getCorrectAnswer = (n) => (isEven(n) ? 'yes' : 'no');
-
 const gameDiscription = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
 const even = () => {
   const randomNumber = randomNumberGenerator(200) + 1;
-  const correctAnswer = getCorrectAnswer(randomNumber);
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
   const question = `Question: ${randomNumber}`;
   return [question, correctAnswer];
 };
